b0ac006f28b59475973c36723a65ef1ad8415e81
Михаил <stulstoldevyat@gmail.com>
2022-02-27 20:40:42 +0300

    Добавьте файлы проекта.

---
+++ Lab19/Computer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab19
+{
+    class Computer
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string TypeProcessor { get; set; }
+        public int Frequency { get; set; }
+        public int VolumeRAM { get; set; }
+        public int VolumeDisk { get; set; } 
+        public int VolumeVideoCard { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+
+        
+
+        //public Computer(int code, string name, string typeProcessor, int frequency, int volumeRAM, int volumeDisk, int volumeVideoCard, double price, int count)
+        //{
+        //    this.Code = code;
+        //    this.Name = name;
+        //    this.TypeProcessor = typeProcessor;
+        //    this.Frequency = frequency;
+        //    this.VolumeRAM = volumeRAM;
+        //    this.VolumeDisk = volumeDisk;
+        //    this.VolumeVideoCard = volumeVideoCard;
+        //    this.Price = price;
+        //    this.Count = count;
+        //}
+    }
+}
---
+++ Lab19/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 6;            
+            List<Computer> listComputer = new List<Computer>()
+            {
+                new Computer(){Code=1,Name="Samsung",TypeProcessor="AMD",Frequency =2000,VolumeRAM=16,VolumeDisk=1024,VolumeVideoCard=3000,Price =44999.50,Count =25},
+                new Computer(){Code=2,Name="Acer",TypeProcessor="Intel",Frequency =1500,VolumeRAM=8,VolumeDisk=2048,VolumeVideoCard=2000,Price =49999.50,Count =40},
+                new Computer(){Code=3,Name="HP",TypeProcessor="AMD",Frequency =1000,VolumeRAM=8,VolumeDisk=512,VolumeVideoCard=1500,Price =44999.50,Count =10},
+                new Computer(){Code=4,Name="Lenovo",TypeProcessor="Intel",Frequency =2500,VolumeRAM=16,VolumeDisk=512,VolumeVideoCard=1000,Price =54999.50,Count =15},
+                new Computer(){Code=5,Name="Dell",TypeProcessor="Intel",Frequency =2300,VolumeRAM=4,VolumeDisk=1024,VolumeVideoCard=2500,Price =39999.50,Count =20},
+                new Computer(){Code=6,Name="Huawei",TypeProcessor="AMD",Frequency =3000,VolumeRAM=4,VolumeDisk=256,VolumeVideoCard=3500,Price =34999.50,Count =31},
+                new Computer(){Code=7,Name="Asus",TypeProcessor="AMD",Frequency =3000,VolumeRAM=4,VolumeDisk=256,VolumeVideoCard=3500,Price =44999.50,Count =50},
+                new Computer(){Code=8,Name="Samsung",TypeProcessor="Intel",Frequency =3000,VolumeRAM=4,VolumeDisk=256,VolumeVideoCard=3500,Price =64999.50,Count =30},
+                new Computer(){Code=9,Name="Xiaomi",TypeProcessor="Intel",Frequency =3000,VolumeRAM=4,VolumeDisk=256,VolumeVideoCard=3500,Price =54999.50,Count =12}
+            };
+
+            Console.WriteLine("Введите тип процессора");
+            string type = Console.ReadLine();
+            List<Computer> computer1 = listComputer.Where(x => x.TypeProcessor == type).ToList();
+            Print(computer1);
+
+            Console.WriteLine("Введите объём ОЗУ");
+            int ram = Convert.ToInt32(Console.ReadLine());
+            List<Computer> computer2 = listComputer.Where(x => x.VolumeRAM >= ram).ToList();
+            Print(computer1);
+
+            // Список, отсортированный по увеличению стоимости 
+            var sortPrice = listComputer
+                .OrderBy(d => d.Price)
+                .ToList();
+            Print(sortPrice);
+            
+
+            // Список, сгруппированный по типу процессора
+            var typeProc = listComputer
+                .Where (d=>d.TypeProcessor=="AMD")
+                .ToList();
+            Print(typeProc);
+            
+
+            // Нахождение компьютера с максимальной ценной
+            var max = listComputer
+                .OrderByDescending(d => d.Price)
+                .FirstOrDefault();
+            Console.WriteLine($"{max.Code} {max.Name} {max.TypeProcessor} {max.Frequency} {max.VolumeRAM} {max.VolumeDisk} {max.VolumeVideoCard} {max.Price} {max.Count}\n");
+            
+
+            // Нахождение компьютера с минимальной ценной
+            var min = listComputer
+                .OrderBy(d => d.Price)
+                .FirstOrDefault();
+            Console.WriteLine($"{min.Code} {min.Name} {min.TypeProcessor} {min.Frequency} {min.VolumeRAM} {min.VolumeDisk} {min.VolumeVideoCard} {min.Price} {min.Count}\n");
+
+            // Нахождение списка компьютеров с количеством не менее 30 штук
+            var count = listComputer
+                .Where(d => d.Count>=30)
+                .ToList();
+            Print(count);
+
+            
+        }
+        static void Print(List<Computer> computers)
+        {
+            foreach (Computer d in computers)
+            {
+                Console.WriteLine($"{d.Code} {d.Name} {d.TypeProcessor} {d.Frequency} {d.VolumeRAM} {d.VolumeDisk} {d.VolumeVideoCard} {d.Price} {d.Count}");
+            }
+            Console.WriteLine();                
+        }
+    }
+}
